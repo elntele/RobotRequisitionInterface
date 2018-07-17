7a6a8ad0bc207662f2893c9cab518e43fe002726
Jorge Candeias <elntele@gmail.com>
2018-07-17 14:36:44 -0300

    comit inicial projeto criado

---
+++ src/main/java/com/digitallworkers/RobotRequisitionInterface/RobotRequisitionInterfaceApplication.java
@@ -0,0 +1,12 @@
+package com.digitallworkers.RobotRequisitionInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotRequisitionInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotRequisitionInterfaceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/digitallworkers/RobotRequisitionInterface/RobotRequisitionInterfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitallworkers.RobotRequisitionInterface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobotRequisitionInterfaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
